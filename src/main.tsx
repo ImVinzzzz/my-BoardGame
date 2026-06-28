763ed3bdae28bd1202c775ad220b476b67b6385d
ImVinzzzz <vricci75@gmail.com>
2026-06-28 15:12:27 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import App from './App';
 import './index.css';
+
+// Aggiungiamo i set di icone alla libreria globale
+library.add(fas, far, fab);
 
 const rootElement = document.getElementById('root');
 
